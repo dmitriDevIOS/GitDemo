fd53f2920cf4444759c50aaa3e0fb211a97d3300
DimaTimoshka <dimatimoshka@gmail.com>
2020-04-03 17:20:46 +0300

    initial commit

---
+++ gitdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitdemo
+//
+//  Created by Dmitrii Timofeev on 03/04/2020.
+//  Copyright © 2020 Dmitrii Timofeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
